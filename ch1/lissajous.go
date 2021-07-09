d3a9dcb79f4a6cf5dae87701512a31868c0e24c9
zackary-clark <zackary.a.clark@gmail.com>
2021-07-09 17:50:39 -0400

    finished 1.4

---
+++ ch1/lissajous.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
+const (
+	blackIndex = 0
+	greenIndex = 1
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	palette := []color.Color{color.Black, color.RGBA{
+		parseArgToUint8(1),
+		parseArgToUint8(2),
+		parseArgToUint8(3),
+		0xff,
+	}}
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+func parseArgToUint8(index int) uint8 {
+	var colorUint uint64
+	if len(os.Args) > 3 {
+		colorUint, _ = strconv.ParseUint(os.Args[index], 10, 32)
+	} else {
+		colorUint = 0xff
+	}
+	return uint8(colorUint)
+}
